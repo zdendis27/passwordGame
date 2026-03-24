6e1fa747c24f7ee801a44d86198d18cc626fb75c
zdendis27 <zdenekvacek007@gmail.com>
2026-03-24 09:15:11 +0100

    Add base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: "/passwordGame/",   // 🔥 tohle přidej
+
   plugins: [react()],
   server: {
     host: true,
